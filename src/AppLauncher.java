541ceabe838f36cf0784e437dc6bb9fbad6f1746
Nour Eldeen Ali Mohamed Ali Hassan El-Khouly <cds.NoureldinAli250403@alexu.edu.eg>
2025-05-02 21:06:15 +0300

    CommonConstants changed

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -10,7 +10,7 @@ public class AppLauncher {
             @Override
             public void run() {
                 // instantiate an LoginFormGUI object and make it visible
-                new LoginFormGUI ().setVisible (true);
+                new AdminGUI ().setVisible (true);
             }
         });
     }
--- src/guis/AdminGUI.java
+++ src/guis/AdminGUI.java
@@ -7,21 +7,6 @@ public class AdminGUI extends Form {
     public AdminGUI() {
         super("Admin");
         this.setSize (1000,800);
-        JLabel titleLabel = new JLabel ("Welcome to Admin Panel");
-//        titleLabel.setFont (new Font ("Dialog", Font.BOLD, 30));
-//        titleLabel.setHorizontalAlignment (SwingConstants.CENTER);
-//        add (titleLabel);
-//        JRadioButton clientRadioButton = new JRadioButton ("Client");
-//        clientRadioButton.setBounds (0, 1, 40, 100);
-//        clientRadioButton.setFont (new Font ("Dialog", Font.BOLD, 20));
-//        JRadioButton adminRadioButton = new JRadioButton ("Admin");
-//        adminRadioButton.setBounds (0, 150, 40, 100);
-//        adminRadioButton.setFont (new Font ("Dialog", Font.BOLD, 20));
-//        ButtonGroup buttonGroup = new ButtonGroup ();
-//        buttonGroup.add (clientRadioButton);
-//        buttonGroup.add (adminRadioButton);
-//        add (clientRadioButton);
-//        add (adminRadioButton);
 
         JTabbedPane tabbedPane = new JTabbedPane ();
         tabbedPane.setBounds (0, 0, 1000, 800);
@@ -37,26 +22,27 @@ public class AdminGUI extends Form {
 
         JLabel activateAccountLabel = new JLabel ("Activate Account");
         activateAccountLabel.setFont (new Font ("Dialog", Font.BOLD, 24));
-        activateAccountLabel.setBounds (400, -20, 200, 100);
+        activateAccountLabel.setBounds (60, -20, 200, 100);
         clientPanel.add (activateAccountLabel);
 
         JLabel username = new JLabel ("Username");
         username.setFont (new Font ("Dialog", Font.BOLD, 20));
-        username.setBounds (0, 30, 100, 30);
+        username.setBounds (5, 60, 100, 30);
         clientPanel.add (username);
 
-        JFormattedTextField usernameTextField = new JFormattedTextField ();
-        usernameTextField.setBounds (100, 30, 200, 100);
+        JTextField usernameTextField = new JTextField ();
+        usernameTextField.setBounds (110, 65, 200, 30);
         clientPanel.add (usernameTextField);
 
         JLabel password = new JLabel ("Password");
         password.setFont (new Font ("Dialog", Font.BOLD, 20));
-        password.setBounds (0, 60, 100, 100);
+        password.setBounds (5, 75, 100, 100);
         clientPanel.add (password);
 
         JPasswordField passwordTextField = new JPasswordField ();
-        usernameTextField.setBounds (100, 60, 200, 100);
-        clientPanel.add (usernameTextField);
-    }
+        passwordTextField.setBounds (110, 115, 200, 30);
+        clientPanel.add (passwordTextField);
 
+        JRadioButton
+    }
 }
