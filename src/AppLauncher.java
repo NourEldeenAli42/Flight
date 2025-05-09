b120811336ae681f904f351abbe06c7ea80ff7e4
MarwanELMahdi <marwan11222007@gmail.com>
2025-05-09 23:30:16 +0300

    Refactor AppLauncher and CreatingNewBooking to use DesignFlightProgram; add PassengerGUI class

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -8,7 +8,7 @@ public class AppLauncher {
             @Override
             public void run() {
                 // instantiate an LoginFormGUI object and make it visible
-                new  LoginFormGUI ().setVisible (true);
+                new DesignFlightProgram (1).setVisible (true);
             }
         });
     }
