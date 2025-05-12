0dc42e115e1910f930bc646b10297531f085a595
MarwanELMahdi <marwan11222007@gmail.com>
2025-05-12 16:39:28 +0300

    Instantiate and display LoginFormGUI in AppLauncher

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -8,6 +8,7 @@ public class AppLauncher {
             @Override
             public void run() {
                 // instantiate an LoginFormGUI object and make it visible
+                new LoginFormGUI ().setVisible (true);
             }
         });
     }
