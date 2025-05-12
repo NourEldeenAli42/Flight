110b9a9fc758628f1ee681e1a31543b0d4ed8ed7
MarwanELMahdi <marwan11222007@gmail.com>
2025-05-12 08:11:59 +0300

    Update AppLauncher.java

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -8,7 +8,6 @@ public class AppLauncher {
             @Override
             public void run() {
                 // instantiate an LoginFormGUI object and make it visible
-                new LoginFormGUI ().setVisible (true);
             }
         });
     }
